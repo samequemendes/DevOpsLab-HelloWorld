88b5975b0f8304e7472255f9989b735486a2c820
Sameque Mendes <samequecm@hotmail.com>
2025-02-02 01:33:09 -0300

    correcao-versao

--- infra/main.tf
+++ infra/main.tf
@@ -13,7 +13,7 @@ resource "aws_elastic_beanstalk_application" "app" {
 resource "aws_elastic_beanstalk_environment" "env" {
   name                = "${var.app_name}-env"
   application         = aws_elastic_beanstalk_application.app.name
-  solution_stack_name = "64bit Amazon Linux 2 v3.5.4 running ${var.app_language} ${var.app_language_version}"
+  solution_stack_name = "64bit Amazon Linux 2023 v4.0.3 running Python 3.8"
 
   setting {
     namespace = "aws:elasticbeanstalk:environment"
